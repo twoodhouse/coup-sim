d518804f42ca08ba4ef3c927b6352be2abf07b25
Trevor Woodhouse <twoodhouse@email.arizona.edu>
2015-08-29 14:09:21 -0700

    Finished log interface - See log_test.go for example - Should be nice and useful for the higher level controller

---
+++ model/log/log.go
@@ -0,0 +1,115 @@
+package log
+
+import "encoding/json"
+// import "fmt"
+
+type Entity struct {
+  jsonStr string
+}
+
+/*
+We want data to be added dynamically to the structure
+For example, a function CreateTurn should create one element in the array
+*/
+
+func New() Entity {
+  var entity = Entity {
+    "[{\"playerName\": null, \"action\": {\"name\": null}}]",
+  }
+  return entity
+}
+
+func (entity *Entity) JsonStr() string {
+  return entity.jsonStr
+}
+
+func (entity *Entity) NextTurn() {
+  entity.jsonStr = entity.jsonStr[:len(entity.jsonStr) - 1] + ",{\"playerName\": null, \"action\": {\"name\": null}}]"
+}
+
+func (entity *Entity) SetPlayerName(playerName string) {
+  log := unmarshalJsonArray(entity.jsonStr)
+  logTurn := log[len(log) - 1]
+  logTurn["playerName"] = playerName
+  marshaledJson, _ := json.Marshal(log)
+  entity.jsonStr = (string(marshaledJson))
+}
+
+func (entity *Entity) SetActionName(actionName string) {
+  log := unmarshalJsonArray(entity.jsonStr)
+  logTurn := log[len(log) - 1]
+  action := logTurn["action"].(map[string]interface{})
+  action["name"] = actionName
+  marshaledJson, _ := json.Marshal(log)
+  entity.jsonStr = (string(marshaledJson))
+}
+
+func (entity *Entity) CreateTarget(target string) {
+  log := unmarshalJsonArray(entity.jsonStr)
+  logTurn := log[len(log) - 1]
+  action := logTurn["action"].(map[string]interface{})
+  action["target"] = target
+  marshaledJson, _ := json.Marshal(log)
+  entity.jsonStr = (string(marshaledJson))
+}
+
+func (entity *Entity) CreateChallenge(challenger string, success bool) {
+  log := unmarshalJsonArray(entity.jsonStr)
+  logTurn := log[len(log) - 1]
+  var successStr string
+  if success {
+    successStr = "true"
+  } else {
+    successStr = "false"
+  }
+  logTurn["challenge"] = unmarshalJson("{\"challenger\": \"" + challenger + "\", \"success\": " + successStr + "}")
+  marshaledJson, _ := json.Marshal(log)
+  entity.jsonStr = (string(marshaledJson))
+}
+
+func (entity *Entity) CreateBlock() {
+  log := unmarshalJsonArray(entity.jsonStr)
+  logTurn := log[len(log) - 1]
+  logTurn["block"] = unmarshalJson("{}")
+  marshaledJson, _ := json.Marshal(log)
+  entity.jsonStr = (string(marshaledJson))
+}
+
+func (entity *Entity) CreateBlockChallenge(challengeSuccess bool) {
+  log := unmarshalJsonArray(entity.jsonStr)
+  logTurn := log[len(log) - 1]
+  block := logTurn["block"].(map[string]interface{})
+  var successStr string
+  if challengeSuccess {
+    successStr = "true"
+  } else {
+    successStr = "false"
+  }
+  block["challengeSuccess"] = successStr
+  marshaledJson, _ := json.Marshal(log)
+  entity.jsonStr = (string(marshaledJson))
+}
+
+func unmarshalJsonArray(str string) []map[string]interface{} {
+  byt := []byte(str)
+  var dat []map[string]interface{}
+  if err := json.Unmarshal(byt, &dat); err != nil {
+    panic(err)
+  }
+  return dat
+}
+
+func unmarshalJson(str string) map[string]interface{} {
+  byt := []byte(str)
+  var dat map[string]interface{}
+  if err := json.Unmarshal(byt, &dat); err != nil {
+    panic(err)
+  }
+  return dat
+}
+
+func (entity *Entity) PrettyJsonStr() string {
+  log := unmarshalJsonArray(entity.jsonStr)
+  prettyMarshaledJson, _ := json.MarshalIndent(log, "", "    ")
+  return string(prettyMarshaledJson)
+}
---
+++ model/log/log_test.go
@@ -0,0 +1,28 @@
+package log
+import (
+  "testing"
+  "fmt"
+)
+
+func TestLogCreation(t *testing.T) {
+  log := New()
+  log.SetPlayerName("Trevor")
+  log.SetActionName("income")
+  log.NextTurn()
+  log.SetPlayerName("Michael")
+  log.SetActionName("tax")
+  log.CreateChallenge("Trevor", true)
+  log.NextTurn()
+  log.SetPlayerName("Zeuterneuman")
+  log.SetActionName("steal")
+  log.CreateTarget("Trevor")
+  log.NextTurn()
+  log.SetPlayerName("Trevor")
+  log.SetActionName("asssassinate")
+  log.CreateTarget("Michael")
+  log.CreateBlock()
+  log.CreateBlockChallenge(true)
+  fmt.Println(log.PrettyJsonStr())
+  // log.SetActionName("income")
+  // log.NewTurn("Trevor")
+}
